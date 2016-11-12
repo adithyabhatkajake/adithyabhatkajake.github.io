10629ab2411bf860595d1ce6b22637198a9e2110
adithyabhatkajake <dth.bht@gmail.com>
2016-11-12 13:41:25 +0530

    clean

--- reg.js
+++
@@ -1,3 +0,0 @@
-function myfunc() {
-	window.alert("You clicked:P");
-}
--- register/write.php
+++
@@ -1,6 +0,0 @@
-<?php
-		$file = fopen("results.txt","a");
-		fwrite($file,$_POST["name1"]." ".$_POST["roll1"]." ".$_POST["name2"]." ".$_POST["roll2"]." ".$_POST["link"]."\n");
-		fclose($file);
-		
-?>
